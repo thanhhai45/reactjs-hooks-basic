2f9496ab45d622c772389922b21dbb1916d87376
thanhhai45 <thanhhai82451495@gmail.com>
2020-07-23 00:13:26 +0700

    Demo-TodoList-for-React-Hooks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
-import ColorBox from './components/ColorBox';
+// import ColorBox from './components/ColorBox';
+import TodoList from './components/TodoList';
 
 function App() {
+  const [todolist, setTodoList] = useState([
+    { id: 1, title: 'Check Learn Reactjs Hook' },
+    { id: 2, title: 'Check Learn Reactjs Redux Hook' },
+    { id: 3, title: 'Check Learn Reactjs Router Hook'},
+  ]);
+
+  function handleTodoClick(todo) {
+    console.log(todo);
+    const index = todolist.findIndex(x => x.id === todo.id)
+    if (index < 0) return;
+
+    const newTodoList = [...todolist];
+    newTodoList.splice(index, 1);
+    setTodoList(newTodoList);
+  }
+
   return (
     <div className="app">
-      <h1>Welcome React Hook Basic</h1>
-      <ColorBox />
+      <h1>React hooks - TodoList</h1>
+      {/* <ColorBox /> */}
+      <TodoList todos={todolist} onTodoClick={handleTodoClick} />
     </div>
   );
 }
